f6a57d2b7ecc698743157a2f1548e8a849feba48
KimSehoon <shggm2000@gmail.com>
2023-09-17 15:36:37 +0900

    init: 프로젝트 세팅

---
+++ src/main/java/com/example/team2_be/Team2BeApplication.java
@@ -0,0 +1,13 @@
+package com.example.team2_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team2BeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team2BeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/team2_be/Team2BeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.team2_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team2BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
