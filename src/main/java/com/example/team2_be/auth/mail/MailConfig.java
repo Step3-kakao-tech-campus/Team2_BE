a30c93d7d6e2ce4d6e6db7cf87b1de23cb1f1b50
MinseokGo <rhalstjr1999@naver.com>
2023-09-18 13:57:28 +0900

    feat: smtp 서버 인증 정보 등록

--- src/main/java/com/example/team2_be/auth/mail/MailConfig.java
+++ src/main/java/com/example/team2_be/auth/mail/MailConfig.java
@@ -21,4 +21,16 @@ public class MailConfig {
 
         return javaMailSender;
     }
+
+    private Properties getMailProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("mail.transport.protocol", "smtp");
+        properties.setProperty("mail.smtp.auth", "true");
+        properties.setProperty("mail.smtp.starttls.enable", "true");
+        properties.setProperty("mail.debug", "true");
+        properties.setProperty("mail.smtp.ssl.trust","smtp.naver.com");
+        properties.setProperty("mail.smtp.ssl.enable","true");
+
+        return properties;
+    }
 }
