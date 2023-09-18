48c9eaaf08cb76ff77359d5a4d34697e2f2fe6a2
MinseokGo <rhalstjr1999@naver.com>
2023-09-18 12:17:22 +0900

    feat: API 응답 실패 메서드 작성

--- src/main/java/com/example/team2_be/core/utils/ApiUtils.java
+++ src/main/java/com/example/team2_be/core/utils/ApiUtils.java
@@ -2,10 +2,15 @@ package com.example.team2_be.core.utils;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 public class ApiUtils {
     public static <T> ApiResult<T> success(T response) {
         return new ApiResult<>(true, response, null);
+    }
+
+    public static ApiResult<?> error(String message, HttpStatus status) {
+        return new ApiResult<>(false, null, new ApiError(message, status.value()));
     }
 
     @Getter
