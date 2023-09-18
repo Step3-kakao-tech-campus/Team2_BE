3ec67600b025b0901430dbfa42996443fa72e907
MinseokGo <rhalstjr1999@naver.com>
2023-09-18 13:54:56 +0900

    feat: 인증코드를 랜덤으로 생성해주는 메서드 작성

---
+++ src/main/java/com/example/team2_be/auth/mail/MailService.java
@@ -0,0 +1,36 @@
+package com.example.team2_be.auth.mail;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Random;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MailService {
+    public String createKey() {
+        StringBuilder key = new StringBuilder();
+        Random rnd = new Random();
+
+        for (int i = 0; i < 8; i++) {
+            int index = rnd.nextInt(3);
+
+            switch(index) {
+                case 0:
+                    key.append((char) (rnd.nextInt(26) + 97));
+                    break;
+                case 1:
+                    key.append((char) (rnd.nextInt(26) + 65));
+                    break;
+                case 2:
+                    key.append((rnd.nextInt(10)));
+                    break;
+            }
+        }
+
+        return key.toString();
+    }
+}
