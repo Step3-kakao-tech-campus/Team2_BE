19fb4aaa27c87b6b5cb4354741f64048954be79d
MinseokGo <rhalstjr1999@naver.com>
2023-09-18 14:24:06 +0900

    feat: redis 서버에 저장된 객체를 가져오는 메소드 작성

--- src/main/java/com/example/team2_be/core/utils/RedisUtils.java
+++ src/main/java/com/example/team2_be/core/utils/RedisUtils.java
@@ -12,6 +12,11 @@ import java.time.Duration;
 public class RedisUtils {
     private final StringRedisTemplate stringRedisTemplate;
 
+    public String getData(String key) {
+        ValueOperations<String,String> valueOperations = stringRedisTemplate.opsForValue();
+        return valueOperations.get(key);
+    }
+
     public void setDataExpire(String key, String value, long duration) {
         ValueOperations<String, String> valueOperations = stringRedisTemplate.opsForValue();
         Duration expireDuration = Duration.ofSeconds(duration);
