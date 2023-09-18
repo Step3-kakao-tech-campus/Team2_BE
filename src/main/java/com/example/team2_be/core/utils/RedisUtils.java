b9ee9fccce06c01d470b1d42ecd0ea9ec1b79837
MinseokGo <rhalstjr1999@naver.com>
2023-09-18 14:22:46 +0900

    feat: redis 스트링 객체 저장 메서드
    만료 기간이 지나면 redis 서버에서 자동 삭제 됩니다.

---
+++ src/main/java/com/example/team2_be/core/utils/RedisUtils.java
@@ -0,0 +1,20 @@
+package com.example.team2_be.core.utils;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+
+@Component
+@RequiredArgsConstructor
+public class RedisUtils {
+    private final StringRedisTemplate stringRedisTemplate;
+
+    public void setDataExpire(String key, String value, long duration) {
+        ValueOperations<String, String> valueOperations = stringRedisTemplate.opsForValue();
+        Duration expireDuration = Duration.ofSeconds(duration);
+        valueOperations.set(key, value, expireDuration);
+    }
+}
