06c05a5b62d1697af962b27eaed833d4bef5565d
JB <wjdqls6704@naver.com>
2023-09-28 02:10:14 +0900

    feat: 유저 정보 추상 클래스 작성

---
+++ src/main/java/com/example/team2_be/auth/oauth2/OAuth2UserInfo.java
@@ -0,0 +1,20 @@
+package com.example.team2_be.auth.oauth2;
+
+import java.util.Map;
+
+public abstract class OAuth2UserInfo {
+    protected Map<String, Object> attributes;
+
+    public OAuth2UserInfo(Map<String, Object> attributes){
+        this.attributes = attributes;
+    }
+
+    // 소셜 식별 값
+    public abstract String getId();
+
+    public abstract String getNickname();
+
+    public abstract String getImageUrl();
+
+    public abstract String getEmail();
+}
