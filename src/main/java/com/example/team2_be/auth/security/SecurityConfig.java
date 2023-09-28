4af3f53d367fecda2550e4f3a9674a3c4f8fd263
KimSehoon <shggm2000@gmail.com>
2023-09-28 22:44:50 +0900

    feat: Security Config 설정
    - Security Config 설정
    - 인증, 권한 실패 처리 미구현

---
+++ src/main/java/com/example/team2_be/auth/security/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.example.team2_be.auth.security;
+
+import com.example.team2_be.auth.AuthTokenProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final AuthTokenProvider authTokenProvider;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        JwtAuthenticationFilter jwtAuthFilter = new JwtAuthenticationFilter(authTokenProvider);
+
+        http
+                .authorizeRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated().and() // 해당 요청을 인증된 사용자만 사용 가능
+                .headers()
+                .frameOptions()
+                .sameOrigin().and()
+                .cors().configurationSource(configurationSource()).and()
+                .csrf().disable()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+        // 인증, 권한 실패 처리 미구현
+    }
+
+    private CorsConfigurationSource configurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*"); // GET, POST, PUT, DELETE (Javascript 요청 허용)
+        configuration.addAllowedOriginPattern("*"); // 모든 IP 주소 허용 (프론트 앤드 IP만 허용 react)
+        configuration.setAllowCredentials(true); // 클라이언트에서 쿠키 요청 허용
+        configuration.addExposedHeader("Authorization"); // 옛날에는 디폴트 였다. 지금은 아닙니다.
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
