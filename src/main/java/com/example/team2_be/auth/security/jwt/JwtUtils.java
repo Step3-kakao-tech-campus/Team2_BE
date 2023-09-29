a72bb5d7966debd601a024ff408dc3bdb0adedaa
won0104 <98508955+won0104@users.noreply.github.com>
2023-09-29 16:39:28 +0900

    feat: JWT 토큰 유효성 검사 메서드

--- src/main/java/com/example/team2_be/auth/security/jwt/JwtUtils.java
+++ src/main/java/com/example/team2_be/auth/security/jwt/JwtUtils.java
@@ -2,15 +2,20 @@ package com.example.team2_be.auth.security.jwt;
 
 import com.example.team2_be.user.Role;
 import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 
+import lombok.extern.log4j.Log4j2;
 import java.security.Key;
 
+@Log4j2
 public class JwtUtils {
-    private final Key key;
+    private static Key key ;
+    //private static BasicLogger log;
 
     public JwtUtils(@Value("${jwt.secret-key}") String secretKey) {
         //시크릿 키를 바탕으로 Key 객체 생성
@@ -19,7 +24,7 @@ public class JwtUtils {
         this.key = Keys.hmacShaKeyFor(keyBytes);
     }
 
-    private  Claims getClaimsFormToken(String token) {
+    private static Claims getClaimsFormToken(String token) {
         return Jwts.parser().setSigningKey(key)
                 .parseClaimsJws(token).getBody();
     }
@@ -33,5 +38,31 @@ public class JwtUtils {
         Claims claims = getClaimsFormToken(token);
         return (Role) claims.get("role");
     }
+
+    public static boolean isValidToken(String token) {
+        //토큰 유효성 검사
+        try {
+            Claims claims = getClaimsFormToken(token);
+            log.info("expireTime :" + claims.getExpiration());
+            log.info("email :" + claims.get("email"));
+            log.info("role :" + claims.get("role"));
+            return true;
+
+        } catch (ExpiredJwtException exception) {
+            log.error("만료된 토큰입니다");
+            return false;
+        } catch (JwtException exception) {
+            log.error("변조된 토큰입니다");
+            return false;
+        } catch (NullPointerException exception) {
+            log.error("빈 토큰입니다");
+            return false;
+        }
+    }
+
+    public static String getTokenFromHeader(String header) {
+        return header.split(" ")[1];
+    }
+
 
 }
