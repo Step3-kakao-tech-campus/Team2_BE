1ad7bc60b81de5522139c09bd92ceb5250cdd7dd
MinseokGo <rhalstjr1999@naver.com>
2023-10-01 15:25:35 +0900

    feat: 유저 정보 조회 컨트롤러 메서드 작성

--- src/main/java/com/example/team2_be/user/UserController.java
+++ src/main/java/com/example/team2_be/user/UserController.java
@@ -1,4 +1,24 @@
 package com.example.team2_be.user;
 
+import com.example.team2_be.auth.security.CustomUserDetails;
+import com.example.team2_be.core.utils.ApiUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/users")
 public class UserController {
+    private final UserService userService;
+
+    @GetMapping("/find")
+    public ResponseEntity<?> find(@AuthenticationPrincipal CustomUserDetails userDetails) {
+        UserResponse.FindDTO findDTO = userService.findUser(userDetails.getUser());
+
+        return ResponseEntity.ok(ApiUtils.success(findDTO));
+    }
 }
