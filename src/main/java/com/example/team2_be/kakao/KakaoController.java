ca28f7d6090756d6ba41427204c9e23fa0850438
KimSehoon <shggm2000@gmail.com>
2023-10-01 13:33:45 +0900

    fix: kakaoLogin method redirect 수정
    - @Controller로 변경 후 redirect 수정

--- src/main/java/com/example/team2_be/kakao/KakaoController.java
+++ src/main/java/com/example/team2_be/kakao/KakaoController.java
@@ -3,14 +3,16 @@ package com.example.team2_be.kakao;
 import com.example.team2_be.core.utils.ApiUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
 @RequiredArgsConstructor
-@RestController
+@Controller
 public class KakaoController {
 
     private final KakaoService kakaoService;
@@ -18,11 +20,11 @@ public class KakaoController {
     @GetMapping("/auth/login")
     public String kakaoLogin(){
         String authCodeUrl = kakaoService.getAuthCodeUrl();
-        return "forward:" + authCodeUrl;
+        return "redirect:" + authCodeUrl;
     }
 
     @GetMapping("/callback")
-    public ResponseEntity<?> getKakaoAccount(@RequestParam("code") String code){
+    public @ResponseBody ResponseEntity<?> getKakaoAccount(@RequestParam("code") String code){
         return ResponseEntity.ok(ApiUtils.success(kakaoService.login(code)));
     }
 }
