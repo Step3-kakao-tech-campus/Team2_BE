5b40d3a6b47a521d1cb55cd8b6a6aa4f6842e7cc
MinseokGo <rhalstjr1999@naver.com>
2023-10-01 15:44:26 +0900

    feat: 유저 정보 수정 요청 DTO 작성

--- src/main/java/com/example/team2_be/user/UserRequest.java
+++ src/main/java/com/example/team2_be/user/UserRequest.java
@@ -1,4 +1,11 @@
 package com.example.team2_be.user;
 
+import lombok.Getter;
+
 class UserRequest {
+    @Getter
+    public static class UpdateDTO {
+        // 요청에 담긴 데이터의 유효성 판별 필요
+        private String newNickname;
+    }
 }
