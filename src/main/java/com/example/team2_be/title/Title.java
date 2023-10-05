ebb5ba95e83229892a025722b49dc995d6435e04
MinseokGo <rhalstjr1999@naver.com>
2023-10-05 20:22:39 +0900

    feat: 칭호 엔티티 클래스 작성

---
+++ src/main/java/com/example/team2_be/title/Title.java
@@ -0,0 +1,33 @@
+package com.example.team2_be.title;
+
+import com.example.team2_be.reward.Reward;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+@Entity
+@Table
+@Getter
+@ToString
+@NoArgsConstructor
+public class Title {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 16, nullable = false)
+    private String titleName;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "reward_id")
+    private Reward reward;
+
+    @Builder
+    public Title(Long id, String titleName, Reward reward) {
+        this.id = id;
+        this.titleName = titleName;
+        this.reward = reward;
+    }
+}
