dee0c549609b994c3303e2dc63b0895bc9806fc7
MinseokGo <rhalstjr1999@naver.com>
2023-10-05 21:02:36 +0900

    feat: 도전과제 진행도 리포지토리 작성

---
+++ src/main/java/com/example/team2_be/reward/progress/ProgressJPARepository.java
@@ -0,0 +1,6 @@
+package com.example.team2_be.reward.progress;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProgressJPARepository extends JpaRepository<Progress, Long> {
+}
