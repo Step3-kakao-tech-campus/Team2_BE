4592fe8ff09b194adba77bde220cfef0fc2f266e
MinseokGo <rhalstjr1999@naver.com>
2023-10-05 18:36:31 +0900

    refactor: 리네임된 클래스 이름으로 리팩터링

--- src/main/java/com/example/team2_be/user/UserController.java
+++ src/main/java/com/example/team2_be/user/UserController.java
@@ -1,7 +1,9 @@
 package com.example.team2_be.user;
 
-import com.example.team2_be.auth.security.CustomUserDetails;
+import com.example.team2_be.core.security.CustomUserDetails;
 import com.example.team2_be.core.utils.ApiUtils;
+import com.example.team2_be.user.dto.UserInfoFindResponseDTO;
+import com.example.team2_be.user.dto.UserInfoUpdateRequestDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -15,13 +17,13 @@ public class UserController {
 
     @GetMapping("/find")
     public ResponseEntity<?> find(@AuthenticationPrincipal CustomUserDetails userDetails) {
-        UserResponse.FindDTO findDTO = userService.findUser(userDetails.getUser());
+        UserInfoFindResponseDTO findDTO = userService.findUser(userDetails.getUser());
 
         return ResponseEntity.ok(ApiUtils.success(findDTO));
     }
 
     @PutMapping("/update")
-    public ResponseEntity<?> update(@RequestBody UserRequest.UpdateDTO updateDTO, @AuthenticationPrincipal CustomUserDetails userDetails) {
+    public ResponseEntity<?> update(@RequestBody UserInfoUpdateRequestDTO updateDTO, @AuthenticationPrincipal CustomUserDetails userDetails) {
         userService.updateUserInfo(updateDTO, userDetails.getUser());
 
         return ResponseEntity.ok(null);
