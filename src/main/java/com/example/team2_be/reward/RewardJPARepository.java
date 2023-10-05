f00c1ad5a3b4c8cfbb81098fbc52329c8bc2d532
MinseokGo <rhalstjr1999@naver.com>
2023-10-05 21:01:25 +0900

    feat: 도전과제 리포지토리 작성

---
+++ src/main/java/com/example/team2_be/reward/RewardJPARepository.java
@@ -0,0 +1,6 @@
+package com.example.team2_be.reward;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RewardJPARepository extends JpaRepository<Reward, Long> {
+}
