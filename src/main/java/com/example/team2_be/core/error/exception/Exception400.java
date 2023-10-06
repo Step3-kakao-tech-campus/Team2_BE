0f79a31f357adfe5d2a90748c353da959f795df5
KimSehoon <shggm2000@gmail.com>
2023-10-07 02:37:38 +0900

    refactor: Exception400 class 리팩토링
    - ApiException class를 상속 받는 형태로 변경

--- src/main/java/com/example/team2_be/core/error/exception/Exception400.java
+++ src/main/java/com/example/team2_be/core/error/exception/Exception400.java
@@ -6,17 +6,13 @@ import org.springframework.http.HttpStatus;
 
 
 @Getter
-public class Exception400 extends RuntimeException {
-
+public class Exception400 extends ApiException {
     public Exception400(String message) {
         super(message);
     }
 
-    public ApiUtils.ApiResult<?> body(){
-        return ApiUtils.error(getMessage(), HttpStatus.BAD_REQUEST);
-    }
-
-    public HttpStatus status(){
+    @Override
+    public HttpStatus getStatus() {
         return HttpStatus.BAD_REQUEST;
     }
 }
