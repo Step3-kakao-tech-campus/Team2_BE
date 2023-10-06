3da63caf03422690e5ba79a7c4cda7ee0f0a16d8
KimSehoon <shggm2000@gmail.com>
2023-10-06 19:32:23 +0900

    feat: exception404 class 생성
    - exception404 class 생성
    - RuntimeException을 상속받음
    - client에게 message와 eunm 값인 HttpStatus.NOT_FOUND 전달 가능

---
+++ src/main/java/com/example/team2_be/core/error/exception/Exception404.java
@@ -0,0 +1,20 @@
+package com.example.team2_be.core.error.exception;
+
+import com.example.team2_be.core.utils.ApiUtils;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class Exception404 extends RuntimeException {
+    public Exception404(String message) {
+        super(message);
+    }
+
+    public ApiUtils.ApiResult<?> body(){
+        return ApiUtils.error(getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    public HttpStatus status(){
+        return HttpStatus.NOT_FOUND;
+    }
+}
