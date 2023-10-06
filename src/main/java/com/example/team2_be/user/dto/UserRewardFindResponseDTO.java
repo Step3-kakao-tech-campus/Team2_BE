b10b8817c7e8ff2932baf928c4aa8c1b32a6bd85
MinseokGo <rhalstjr1999@naver.com>
2023-10-06 16:48:47 +0900

    feat: 도전과제 데이터 가공을  위한 DTO 클래스 작성

--- src/main/java/com/example/team2_be/user/dto/UserRewardFindResponseDTO.java
+++ src/main/java/com/example/team2_be/user/dto/UserRewardFindResponseDTO.java
@@ -19,3 +19,35 @@ public class UserRewardFindResponseDTO {
                 .collect(Collectors.toList());
     }
 }
+
+@Getter
+class RewardDTO {
+    private Long id;
+    private String rewardName;
+    private String description;
+    private String level;
+    private int goalCount;
+    private int count;
+    private char success;
+
+    public RewardDTO(Reward reward, List<Progress> progresses) {
+        this.id = reward.getId();
+        this.rewardName = reward.getRewardName();
+        this.description = reward.getDescription();
+        this.level = reward.getLevel();
+        this.goalCount = reward.getGoalCount();
+
+        Optional<Progress> findProgress = progresses.stream()
+                .filter(progress -> progress.getReward().getId().equals(reward.getId()))
+                .findFirst();
+
+        Progress thisProgress = new Progress();
+
+        if(findProgress.isPresent()) {
+            thisProgress = findProgress.get();
+        }
+
+        this.count = thisProgress.getCount();
+        this.success = thisProgress.getSuccess();
+    }
+}
