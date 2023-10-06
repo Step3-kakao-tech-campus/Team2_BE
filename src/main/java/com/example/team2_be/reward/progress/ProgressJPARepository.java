72012fcde57dc9b4d32aac3100cea34ff56b6478
MinseokGo <rhalstjr1999@naver.com>
2023-10-06 16:46:14 +0900

    feat: 유저 아이디로 도전과제 진행도를 찾는 메서드 작성

--- src/main/java/com/example/team2_be/reward/progress/ProgressJPARepository.java
+++ src/main/java/com/example/team2_be/reward/progress/ProgressJPARepository.java
@@ -1,6 +1,12 @@
 package com.example.team2_be.reward.progress;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface ProgressJPARepository extends JpaRepository<Progress, Long> {
+    @Query("select p from Progress p where p.user.id = :id")
+    List<Progress> findByUserId(@Param("id") Long id);
 }
