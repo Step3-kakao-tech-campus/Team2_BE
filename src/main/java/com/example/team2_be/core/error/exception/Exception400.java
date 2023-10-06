126750acaac1fe33f24b92252e178113fbef931b
KimSehoon <shggm2000@gmail.com>
2023-10-06 19:29:10 +0900

    feat: exception400 class 생성
    - exception400 class 생성
    - RuntimeException을 상속받음
    - client에게 message와 eunm 값인 HttpStatus.BAD_REQUEST 전달 가능

---
+++ src/main/java/com/example/team2_be/core/error/exception/Exception400.java
@@ -0,0 +1,22 @@
+package com.example.team2_be.core.error.exception;
+
+import com.example.team2_be.core.utils.ApiUtils;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+
+@Getter
+public class Exception400 extends RuntimeException {
+
+    public Exception400(String message) {
+        super(message);
+    }
+
+    public ApiUtils.ApiResult<?> body(){
+        return ApiUtils.error(getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    public HttpStatus status(){
+        return HttpStatus.BAD_REQUEST;
+    }
+}
