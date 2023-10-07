7ca7a7eb564d66bffe62b18b911bd51e4af5c21c
JB <wjdqls6704@naver.com>
2023-10-08 02:22:20 +0900

    feat: #22 앨범 엔티티 변경에 따른 메서드 변경
    유저 및 앨범을 이용한 앨범 멤버 생성으로 변경
    찾는 앨범이 없을 시 예외 처리

--- src/main/java/com/example/team2_be/album/member/AlbumMemberService.java
+++ src/main/java/com/example/team2_be/album/member/AlbumMemberService.java
@@ -1,5 +1,8 @@
 package com.example.team2_be.album.member;
 
+import com.example.team2_be.album.Album;
+import com.example.team2_be.album.AlbumJPARepository;
+import com.example.team2_be.core.error.exception.Exception404;
 import com.example.team2_be.user.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,14 +13,19 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class AlbumMemberService {
 
+    private final AlbumJPARepository albumJPARepository;
     private final AlbumMemberJPARepository albumMemberJPARepository;
 
     public void addUser(User user, Long albumId){
+        // 앨범이 없을 경우 예외 처리, 후에 추가 수정
+        Album album = albumJPARepository.findById(albumId)
+                .orElseThrow(() -> new Exception404("해당 앨범이 존재하지 않습니다."));
+
         AlbumMember albumMember = albumMemberJPARepository.findByUserAndGroup(user.getId(), albumId);
         if(albumMember == null) {
             albumMember = AlbumMember.builder()
-                    .user(user.getId())
-                    .group(albumId)
+                    .user(user)
+                    .group(album)
                     .build();
             albumMemberJPARepository.save(albumMember);
         }
