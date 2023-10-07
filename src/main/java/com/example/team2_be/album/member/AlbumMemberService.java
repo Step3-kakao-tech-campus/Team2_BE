eafbef2c4aa606621e4e31f073d0399b064166c1
JB <wjdqls6704@naver.com>
2023-10-08 02:33:17 +0900

    feat: #22 userDetails 대신 userId 사용

--- src/main/java/com/example/team2_be/album/member/AlbumMemberService.java
+++ src/main/java/com/example/team2_be/album/member/AlbumMemberService.java
@@ -4,6 +4,7 @@ import com.example.team2_be.album.Album;
 import com.example.team2_be.album.AlbumJPARepository;
 import com.example.team2_be.core.error.exception.Exception404;
 import com.example.team2_be.user.User;
+import com.example.team2_be.user.UserJPARepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,15 +14,17 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(readOnly = true)
 public class AlbumMemberService {
 
+    private final UserJPARepository userJPARepository;
     private final AlbumJPARepository albumJPARepository;
     private final AlbumMemberJPARepository albumMemberJPARepository;
 
-    public void addUser(User user, Long albumId){
+    public void addUser(Long userId, Long albumId){
+        User user = userJPARepository.getReferenceById(userId);
         // 앨범이 없을 경우 예외 처리, 후에 추가 수정
         Album album = albumJPARepository.findById(albumId)
                 .orElseThrow(() -> new Exception404("해당 앨범이 존재하지 않습니다."));
 
-        AlbumMember albumMember = albumMemberJPARepository.findByUserAndGroup(user.getId(), albumId);
+        AlbumMember albumMember = albumMemberJPARepository.findByUserAndGroup(userId, albumId);
         if(albumMember == null) {
             albumMember = AlbumMember.builder()
                     .user(user)
