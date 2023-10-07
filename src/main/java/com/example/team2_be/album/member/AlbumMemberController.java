9993186759d98d65a44cceb30311d7c8848d4773
JB <wjdqls6704@naver.com>
2023-10-08 04:24:52 +0900

    feat: #22 앨범 멤버 조회 컨트롤러 메서드 작성

--- src/main/java/com/example/team2_be/album/member/AlbumMemberController.java
+++ src/main/java/com/example/team2_be/album/member/AlbumMemberController.java
@@ -1,6 +1,8 @@
 package com.example.team2_be.album.member;
 
 
+import com.example.team2_be.album.dto.AlbumMemberFindResponseDTO;
+import com.example.team2_be.core.utils.ApiUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +13,13 @@ import org.springframework.web.bind.annotation.*;
 public class AlbumMemberController {
 
     private final AlbumMemberService albumMemberService;
+
+    @GetMapping
+    public ResponseEntity<ApiUtils.ApiResult<AlbumMemberFindResponseDTO>> getMembers(@PathVariable Long groupId){
+        AlbumMemberFindResponseDTO albumMemberFindResponseDTO = albumMemberService.findMembers(groupId);
+
+        return ResponseEntity.ok(ApiUtils.success(albumMemberFindResponseDTO));
+    }
 
     @PostMapping("/{userId}")
     public ResponseEntity<Void> addMember(@PathVariable Long groupId, @PathVariable Long userId){
