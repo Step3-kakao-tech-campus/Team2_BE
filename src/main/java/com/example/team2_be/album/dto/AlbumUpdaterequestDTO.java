a9e8fd0f43eb66230011e4684e6a1f37537e6b5a
won0104 <98508955+won0104@users.noreply.github.com>
2023-10-08 16:35:57 +0900

    feat: AlbumUpdaterequestDTO 작성

---
+++ src/main/java/com/example/team2_be/album/dto/AlbumUpdaterequestDTO.java
@@ -0,0 +1,11 @@
+package com.example.team2_be.album.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class AlbumUpdaterequestDTO {
+    private String albumName;
+    private String Description;
+    private String image;
+}
