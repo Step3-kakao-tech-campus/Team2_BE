c7f2a42b880bd064888e55f42872d070b430ec0d
won0104 <98508955+won0104@users.noreply.github.com>
2023-10-08 19:39:04 +0900

    feat:  album findAllByEmail 레포지지토리 수정

--- src/main/java/com/example/team2_be/album/AlbumJPARepository.java
+++ src/main/java/com/example/team2_be/album/AlbumJPARepository.java
@@ -2,7 +2,9 @@ package com.example.team2_be.album;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AlbumJPARepository extends JpaRepository<Album, Long> {
-    Album findALLByEmail(String email);
+    List<Album> findALLByEmail(String email);
     Album findByAlbumId(Long id);
 }
