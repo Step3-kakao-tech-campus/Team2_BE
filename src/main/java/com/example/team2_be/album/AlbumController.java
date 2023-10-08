562cb241796100abb4f7d01e18ca7d7b59390cfb
won0104 <98508955+won0104@users.noreply.github.com>
2023-10-08 18:37:04 +0900

    feat:  album update 컨트롤러 작성

--- src/main/java/com/example/team2_be/album/AlbumController.java
+++ src/main/java/com/example/team2_be/album/AlbumController.java
@@ -1,14 +1,12 @@
 package com.example.team2_be.album;
 
 import com.example.team2_be.album.dto.AlbumCreateRequestDTO;
+import com.example.team2_be.album.dto.AlbumUpdaterequestDTO;
 import com.example.team2_be.core.security.CustomUserDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
@@ -27,4 +25,12 @@ public class AlbumController {
         return ResponseEntity.ok(null);
     }
 
+    // 앨범 수정 기능 PUT
+    // /albums/{id}/update
+    @PutMapping("albums/{id}/update")
+    public ResponseEntity<?> updateAlbums(@RequestBody @Valid AlbumUpdaterequestDTO requestDTO, @AuthenticationPrincipal CustomUserDetails userDetails, @PathVariable Long id){
+        albumService.updateAlbum(requestDTO,userDetails.getUser(),id);
+
+        return ResponseEntity.ok(null);
+    }
 }
