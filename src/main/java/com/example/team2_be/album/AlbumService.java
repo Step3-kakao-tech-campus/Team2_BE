a0a549f0d6cc1b760a5f76d50f1e7d3affec75d9
won0104 <98508955+won0104@users.noreply.github.com>
2023-10-08 18:31:00 +0900

    feat:  album update 서비스 로직 구현

--- src/main/java/com/example/team2_be/album/AlbumService.java
+++ src/main/java/com/example/team2_be/album/AlbumService.java
@@ -1,11 +1,14 @@
 package com.example.team2_be.album;
 
 import com.example.team2_be.album.dto.AlbumCreateRequestDTO;
+import com.example.team2_be.album.dto.AlbumUpdaterequestDTO;
 import com.example.team2_be.user.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -26,6 +29,17 @@ public class AlbumService {
         return newAlbum;
     }
 
+    public Album updateAlbum(AlbumUpdaterequestDTO requestDTO, User user, Long id) {
+        Album album = albumJPARepository.findByAlbumId(id);
+        //유저 확인,,필요
 
+        String updatedAlbumName = requestDTO.getAlbumName() != null ? requestDTO.getAlbumName() : album.getAlbumName();
+        String updatedDescription = requestDTO.getDescription() != null ? requestDTO.getDescription() : album.getDescription();
+        String updatedImage = requestDTO.getImage() != null ? requestDTO.getImage() : album.getImage();
+
+        album.update(updatedAlbumName,updatedDescription,updatedImage);
+
+        return album;
+    }
 
 }
