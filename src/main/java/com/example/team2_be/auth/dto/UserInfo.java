9d18633a39c30d27eeaebb39890ac8d456518c3e
JB <wjdqls6704@naver.com>
2023-10-12 20:06:53 +0900

    feat: #3 UserInfo 작성

---
+++ src/main/java/com/example/team2_be/auth/dto/UserInfo.java
@@ -0,0 +1,17 @@
+package com.example.team2_be.auth.dto;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@ToString
+@Getter
+@NoArgsConstructor
+@JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class UserInfo {
+    private UserAccount userAccount;
+
+    public static UserInfo fail(){return null;}
+}
