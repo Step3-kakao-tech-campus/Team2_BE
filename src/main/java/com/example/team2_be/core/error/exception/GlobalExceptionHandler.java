243774c81f7462725265054e7aa639d46ffb9ec6
KimSehoon <shggm2000@gmail.com>
2023-10-12 19:51:48 +0900

    feat: filterStackTraceOnException method 생성
    - stackTrace가 굉장히 길게 보내질 수 있기 때문에 DB에 Column length 초과 및 오류 확인에 어려움이 있음
    - 패키지 내 class와 관련된 stackTrace만 필터링해서 Error DB에 담는 method

--- src/main/java/com/example/team2_be/core/error/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/team2_be/core/error/exception/GlobalExceptionHandler.java
@@ -1,26 +1,53 @@
 package com.example.team2_be.core.error.exception;
 
 import com.example.team2_be.core.utils.ApiUtils;
+import com.example.team2_be.error.Error;
+import com.example.team2_be.error.ErrorJPARepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    private final ErrorJPARepository errorJPARepository;
 
     @ExceptionHandler(ApiException.class)
     public ResponseEntity<?> handleApiException(ApiException e){
         return new ResponseEntity<>(e.getBody(), e.getStatus());
     }
 
+    @Transactional
     @ExceptionHandler(Exception.class)
     public ResponseEntity<?> handleUnknownServerError(Exception e) {
         ApiUtils.ApiResult<?> apiResult = ApiUtils.error(e.getMessage(),
                 HttpStatus.INTERNAL_SERVER_ERROR);
 
-        //error 테이블을 생성하고 의도치 않은 에러에 대해 관리할 필요가 있음.
+        Error error = Error.builder()
+                .message(e.getMessage())
+                .stackTrace(filterStackTraceOnException(e).toString())
+                .createAt(LocalDateTime.now())
+                .build();
+        errorJPARepository.save(error);
 
         return new ResponseEntity<>(apiResult, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    private StringBuilder filterStackTraceOnException(Exception e) {
+        StackTraceElement[] stackTrace = e.getStackTrace();
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (StackTraceElement element : stackTrace) {
+            if (element.getClassName().contains("com.example.team2_be")) {
+                stringBuilder.append(element).append("\n");
+            }
+        }
+        return stringBuilder;
     }
 }
