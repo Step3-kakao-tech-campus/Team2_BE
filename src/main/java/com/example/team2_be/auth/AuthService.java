dfe84517d5cf52ace13b7057a232eca539a965d6
JB <wjdqls6704@naver.com>
2023-10-12 21:05:03 +0900

    feat: #3 카카오 로그인 메서드 작성

--- src/main/java/com/example/team2_be/auth/AuthService.java
+++ src/main/java/com/example/team2_be/auth/AuthService.java
@@ -1,13 +1,15 @@
 package com.example.team2_be.auth;
 
+import com.example.team2_be.auth.dto.UserAccountDTO;
 import com.example.team2_be.auth.dto.UserTokenDTO;
+import com.example.team2_be.core.security.JwtTokenProvider;
+import com.example.team2_be.user.User;
+import com.example.team2_be.user.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
 
@@ -18,12 +20,11 @@ import java.net.URI;
 public class AuthService {
 
     private final AuthClient client;
+    private final UserService userService;
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Value("${kakao.token-url}")
     private String kakaoTokenUrl;
-
-    @Value("${kakao.auth-url}")
-    private String kakaoAuthUrl;
 
     @Value("${kakao.restapi-key}")
     private String restapiKey;
@@ -34,16 +35,28 @@ public class AuthService {
     @Value("${kakao.user-api-url}")
     private String kakaoUserApiUrl;
 
-    @Autowired
-    private RestTemplate restTemplate;
-
     //https://kauth.kakao.com/oauth/authorize?response_type=code&client_id={여기에 REST API KEY를 입력해주세요}&redirect_uri=http://localhost:8080/callback
-    public UserTokenDTO getKakaoAccesToken(String code){
+    private UserTokenDTO getKakaoAccessToken(String code){
         try {
             return client.getToken(new URI(kakaoTokenUrl), restapiKey, redirectUrl, code, "authorization_code");
         } catch (Exception e) {
-            log.error("토큰 발급 오류");
+            log.error("토큰 발급 오류입니다");
             return UserTokenDTO.fail();
         }
+    }
+
+    public String kakaoLogin(String code){
+        UserTokenDTO userToken = getKakaoAccessToken(code);
+        UserAccountDTO userAccount = null;
+
+        try {
+            userAccount = client.getInfo(new URI(kakaoUserApiUrl), userToken.getTokenType() + " " + userToken.getAccessToken()).getUserAccount();
+        } catch (Exception e) {
+            log.error("유저 정보 확인 오류입니다");
+        }
+
+        User user = userService.checkUser(userAccount);
+
+        return jwtTokenProvider.create(user);
     }
 }
