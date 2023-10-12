85d6cc142a0392c5c330e11f16ac4ffa6304eb68
won0104 <98508955+won0104@users.noreply.github.com>
2023-10-12 18:12:08 +0900

    refactor: import문 수정

--- src/main/java/com/example/team2_be/album/AlbumJPARepository.java
+++ src/main/java/com/example/team2_be/album/AlbumJPARepository.java
@@ -1,8 +1,8 @@
 package com.example.team2_be.album;
 
-import feign.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
