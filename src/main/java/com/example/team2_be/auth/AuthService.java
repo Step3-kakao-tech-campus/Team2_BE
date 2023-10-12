34ae8a55ba2eaf080876fc960bf16cf90f6d8166
JB <wjdqls6704@naver.com>
2023-10-12 19:22:38 +0900

    feat: auth 서비스 작성

---
+++ src/main/java/com/example/team2_be/auth/AuthService.java
@@ -0,0 +1,12 @@
+package com.example.team2_be.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class AuthService {
+    
+}
