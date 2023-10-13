f1f027a7f315cb89fc2ecb2e22759f9aeb0bfa48
won0104 <98508955+won0104@users.noreply.github.com>
2023-10-13 12:05:40 +0900

    refactor: 앨범 생성시 생성 유저를 Member로 자동 추가

--- src/main/java/com/example/team2_be/album/AlbumController.java
+++ src/main/java/com/example/team2_be/album/AlbumController.java
@@ -3,7 +3,9 @@ package com.example.team2_be.album;
 import com.example.team2_be.album.dto.AlbumCreateRequestDTO;
 import com.example.team2_be.album.dto.AlbumFindAllResponseDTO;
 import com.example.team2_be.album.dto.AlbumUpdaterequestDTO;
+import com.example.team2_be.album.member.AlbumMemberService;
 import com.example.team2_be.core.security.CustomUserDetails;
+import com.example.team2_be.user.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -16,25 +18,28 @@ import javax.validation.Valid;
 @RequestMapping("/albums")
 public class AlbumController {
     private final AlbumService albumService;
+    private final AlbumMemberService albumMemberService;
 
-    // 앨범 생성기능 POST "/albums/create"
-    @PostMapping ("/create")
-    public ResponseEntity<?> createAlbum(@RequestBody @Valid AlbumCreateRequestDTO requestDTO, @AuthenticationPrincipal CustomUserDetails userDetails){
-        albumService.createAlbum(requestDTO,userDetails.getUser());
-
+    // 앨범 생성기능 POST "/albums"
+    @PostMapping ("")
+    public ResponseEntity<Void> createAlbum(@RequestBody @Valid AlbumCreateRequestDTO requestDTO, @AuthenticationPrincipal CustomUserDetails userDetails){
+        User user = userDetails.getUser();
+        Album newAlbum = albumService.createAlbum(requestDTO,user);
+        // album을 생성하는 유저를 albumMember로 추가
+        albumMemberService.addMember(newAlbum.getId(),user.getId());
         return ResponseEntity.ok(null);
     }
 
-    // 앨범 수정 기능 PUT "/albums/{id}/update"
-    @PutMapping("/{id}/update")
-    public ResponseEntity<?> updateAlbum (@RequestBody @Valid AlbumUpdaterequestDTO requestDTO, @AuthenticationPrincipal CustomUserDetails userDetails, @PathVariable Long id){
+    // 앨범 수정 기능 PUT "/albums/{albumId}"
+    @PutMapping("/{albumId}")
+    public ResponseEntity<Void> updateAlbum (@RequestBody @Valid AlbumUpdaterequestDTO requestDTO, @AuthenticationPrincipal CustomUserDetails userDetails, @PathVariable Long id){
         albumService.updateAlbum(requestDTO,userDetails.getUser(),id);
         return ResponseEntity.ok(null);
     }
 
     // 앨범 조회 기능 GET "/albums"
     @GetMapping("")
-    public ResponseEntity<?> findAllAlbum (@AuthenticationPrincipal CustomUserDetails userDetails){
+    public ResponseEntity<Void> findAllAlbum (@AuthenticationPrincipal CustomUserDetails userDetails){
         AlbumFindAllResponseDTO responseDTO = albumService.findAllAlbum(userDetails.getUser());
 
         return ResponseEntity.ok(null);
