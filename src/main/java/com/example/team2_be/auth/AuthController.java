252b886cede8f3668a853aab7d50f756cb8ce3be
won0104 <98508955+won0104@users.noreply.github.com>
2023-10-15 21:28:11 +0900

    feat: #3 AuthController 세팅

--- src/main/java/com/example/team2_be/auth/AuthController.java
+++ src/main/java/com/example/team2_be/auth/AuthController.java
@@ -1,4 +1,13 @@
 package com.example.team2_be.auth;
+import com.example.team2_be.core.utils.ApiUtils;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth")
 public class AuthController {
+    private final AuthService authService;
+    
 }
