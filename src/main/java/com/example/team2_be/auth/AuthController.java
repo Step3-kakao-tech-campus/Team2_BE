73b8996949c23681c6c56afec288900248434bcd
won0104 <98508955+won0104@users.noreply.github.com>
2023-10-15 21:28:47 +0900

    feat: #3 카카오 로그인기능  컨트롤러 작성

--- src/main/java/com/example/team2_be/auth/AuthController.java
+++ src/main/java/com/example/team2_be/auth/AuthController.java
@@ -9,5 +9,9 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/auth")
 public class AuthController {
     private final AuthService authService;
-    
+
+    @PostMapping("/kakao/login")
+    public @ResponseBody ResponseEntity<ApiUtils.ApiResult<String>> kakaoLogin(@RequestBody String code){
+        return ResponseEntity.ok(ApiUtils.success(authService.kakaoLogin(code)));
+    }
 }
