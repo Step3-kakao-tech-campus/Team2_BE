f0651bc5cd11309fd707413b796acaeb381830cb
KimSehoon <shggm2000@gmail.com>
2023-10-16 17:35:56 +0900

    style: Exception403 -> ForbiddenException
    - Exception403 class -> ForbiddenException 이름 변경

--- src/main/java/com/example/team2_be/core/error/exception/ForbiddenException.java
+++ src/main/java/com/example/team2_be/core/error/exception/ForbiddenException.java
@@ -1,13 +1,12 @@
 package com.example.team2_be.core.error.exception;
 
-import com.example.team2_be.core.utils.ApiUtils;
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 @Getter
-public class Exception403 extends ApiException{
+public class ForbiddenException extends ApiException{
 
-    public Exception403(String message) {
+    public ForbiddenException(String message) {
         super(message);
     }
 
