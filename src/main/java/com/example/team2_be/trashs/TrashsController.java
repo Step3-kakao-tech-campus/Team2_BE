d12643aef5d769becc7bfcc13a52284ba9541f9e
won0104 <98508955+won0104@users.noreply.github.com>
2023-10-18 12:58:49 +0900

    feat: 휴지통 조회 메서드 생성

--- src/main/java/com/example/team2_be/trashs/TrashsController.java
+++ src/main/java/com/example/team2_be/trashs/TrashsController.java
@@ -1,6 +1,11 @@
 package com.example.team2_be.trashs;
 
+import com.example.team2_be.core.security.CustomUserDetails;
+import com.example.team2_be.core.utils.ApiUtils;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +13,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/albums/{albumId}/trashs")
 public class TrashsController {
+
+    //휴지통 조회 GET
+    @GetMapping("/")
+    public ResponseEntity<ApiUtils.ApiResult> findTrashs(@AuthenticationPrincipal CustomUserDetails userDetails){
+        Long userId = userDetails.getUser().getId();
+
+    }
 
 }
