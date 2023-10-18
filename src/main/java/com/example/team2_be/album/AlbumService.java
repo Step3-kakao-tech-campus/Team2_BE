85fc4311d03760bee817e3900594a32c71090c75
won0104 <98508955+won0104@users.noreply.github.com>
2023-10-18 09:46:31 +0900

    refactor: AlbumService 예외처리 이름 변경

--- src/main/java/com/example/team2_be/album/AlbumService.java
+++ src/main/java/com/example/team2_be/album/AlbumService.java
@@ -3,7 +3,7 @@ package com.example.team2_be.album;
 import com.example.team2_be.album.dto.AlbumCreateRequestDTO;
 import com.example.team2_be.album.dto.AlbumFindAllResponseDTO;
 import com.example.team2_be.album.dto.AlbumUpdaterequestDTO;
-import com.example.team2_be.core.error.exception.Exception404;
+import com.example.team2_be.core.error.exception.NotFoundException;
 import com.example.team2_be.user.User;
 import com.example.team2_be.user.UserJPARepository;
 import lombok.RequiredArgsConstructor;
@@ -38,7 +38,7 @@ public class AlbumService {
     @Transactional
     public Album updateAlbum(AlbumUpdaterequestDTO requestDTO, Long AlbumId) {
         Album album = albumJPARepository.findById(AlbumId)
-                .orElseThrow(() -> new Exception404("해당 id값을 가진 앨범을 찾을 수 없습니다. : " + AlbumId));
+                .orElseThrow(() -> new NotFoundException("해당 id값을 가진 앨범을 찾을 수 없습니다. : " + AlbumId));
 
         String updatedAlbumName = requestDTO.getAlbumName() != null ? requestDTO.getAlbumName() : album.getAlbumName();
         String updatedDescription = requestDTO.getDescription() != null ? requestDTO.getDescription() : album.getDescription();
@@ -52,7 +52,7 @@ public class AlbumService {
     // 앨범 조회 기능
     public AlbumFindAllResponseDTO findAllAlbum (Long userId){
         User findUser = userJPARepository.findById(userId)
-                .orElseThrow(() -> new Exception404("해당 유저를 찾을 수 없습니다."));
+                .orElseThrow(() -> new NotFoundException("해당 유저를 찾을 수 없습니다."));
 
         List<Album> albums = albumJPARepository.findAllByUserId(userId);
 
