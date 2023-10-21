e7ee6bb8948aa8035137bdcfc52619f317e7c117
JB <wjdqls6704@naver.com>
2023-10-21 16:34:36 +0900

    feat: #52 휴지통 조회 메서드 작성

--- src/main/java/com/example/team2_be/trash/trashService.java
+++ src/main/java/com/example/team2_be/trash/trashService.java
@@ -1,4 +1,22 @@
 package com.example.team2_be.trash;
 
+import com.example.team2_be.trash.dto.TrashesFindResponseDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class trashService {
+
+    private final TrashJPARepository trashJPARepository;
+
+    public TrashesFindResponseDTO findAll(Long albumId){
+        List<Trash> trashes = trashJPARepository.findAllByAlbumId(albumId);
+
+        return new TrashesFindResponseDTO(albumId, trashes);
+    }
 }
