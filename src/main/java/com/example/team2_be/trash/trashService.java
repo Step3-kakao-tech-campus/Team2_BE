198a75d51bc67e569d85de0905de981629ec79c3
JB <wjdqls6704@naver.com>
2023-10-21 17:05:03 +0900

    feat: #52 앨범 페이지 복구 메서드 작성

--- src/main/java/com/example/team2_be/trash/trashService.java
+++ src/main/java/com/example/team2_be/trash/trashService.java
@@ -1,5 +1,6 @@
 package com.example.team2_be.trash;
 
+import com.example.team2_be.core.error.exception.NotFoundException;
 import com.example.team2_be.trash.dto.TrashesFindResponseDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -18,5 +19,12 @@ public class trashService {
         List<Trash> trashes = trashJPARepository.findAllByAlbumId(albumId);
 
         return new TrashesFindResponseDTO(albumId, trashes);
+    }
+
+    public void restoreTrash(Long trashId){
+        Trash trash = trashJPARepository.findById(trashId)
+                .orElseThrow(() -> new NotFoundException("해당 페이지가 휴지통 내에 존재하지 않습니다."));
+
+        trashJPARepository.delete(trash);
     }
 }
