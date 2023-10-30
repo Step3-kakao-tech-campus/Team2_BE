14585878de59051c7f0597bf27ebb76c36968f15
won0104 <98508955+won0104@users.noreply.github.com>
2023-10-30 15:08:15 +0900

    feat: purgeTrash 메서드 구현

--- src/main/java/com/example/team2_be/trash/TrashService.java
+++ src/main/java/com/example/team2_be/trash/TrashService.java
@@ -5,6 +5,7 @@ import com.example.team2_be.core.error.exception.NotFoundException;
 import com.example.team2_be.trash.dto.TrashesFindResponseDTO;
 import com.example.team2_be.user.User;
 import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.TaskScheduler;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,6 +17,7 @@ import java.util.List;
 public class TrashService {
 
     private final TrashJPARepository trashJPARepository;
+    private final TaskScheduler taskScheduler;
 
     public TrashesFindResponseDTO findTrashes(Long albumId){
         List<Trash> trashes = trashJPARepository.findAllByAlbumId(albumId);
@@ -29,5 +31,20 @@ public class TrashService {
                 .orElseThrow(() -> new NotFoundException("해당 페이지가 휴지통 내에 존재하지 않습니다."));
 
         trashJPARepository.delete(trash);
+    }
+
+    @Transactional
+    public void purgeTrash(Long trashId){
+        trashJPARepository.deleteById(trashId);
+    }
+
+    @Transactional
+    public Trash createTrash(User user, AlbumPage albumPage){
+        final Trash newTrash = trashJPARepository.save(Trash.builder()
+                .user(user)
+                .albumPage(albumPage)
+                .build());
+
+        return newTrash;
     }
 }
