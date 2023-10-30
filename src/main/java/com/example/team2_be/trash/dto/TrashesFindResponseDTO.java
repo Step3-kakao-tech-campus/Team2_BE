3a6200be7ff31a451dc9df8aa518c9311e672911
won0104 <98508955+won0104@users.noreply.github.com>
2023-10-30 15:09:37 +0900

    refactor : deleteAt 삭제

--- src/main/java/com/example/team2_be/trash/dto/TrashesFindResponseDTO.java
+++ src/main/java/com/example/team2_be/trash/dto/TrashesFindResponseDTO.java
@@ -33,7 +33,6 @@ public class TrashesFindResponseDTO {
             // 휴지통 페이지 미리보기 이미지 추가
             this.deleter = trash.getUser().getNickname();
             this.createAt = trash.getCreateAt();
-            this.deleteAt = trash.getDeleteAt();
         }
     }
 }
