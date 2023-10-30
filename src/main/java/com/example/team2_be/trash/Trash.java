c48e123f80d535255ae864b94c038ee36698c783
won0104 <98508955+won0104@users.noreply.github.com>
2023-10-30 14:35:24 +0900

    refactor: TaskScheduler 사용으로 인한 deleteAt 필드 삭제

--- src/main/java/com/example/team2_be/trash/Trash.java
+++ src/main/java/com/example/team2_be/trash/Trash.java
@@ -8,8 +8,9 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import javax.persistence.*;
-import java.time.LocalDateTime;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity
 @Getter
@@ -24,16 +25,11 @@ public class Trash extends BaseEntity {
     @JoinColumn (name ="albumPage_id",nullable = false)
     private AlbumPage albumPage;
 
-
-    @Column (nullable = false)
-    private LocalDateTime deleteAt;
-
     @Builder
     public Trash(Long id, User user, AlbumPage albumPage) {
         super(id);
         this.user = user;
         this.albumPage =albumPage;
-        // 앨범 페이지 삭제 시간 - 여기서 할지 serviced에서 할지 테스트 필요
-        this.deleteAt = this.getCreateAt().plusDays(7);
     }
 }
+
