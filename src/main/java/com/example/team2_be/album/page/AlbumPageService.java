89b3b77df9739da4f5ead1a8673a53b228a062bb
KimSehoon <shggm2000@gmail.com>
2023-11-05 00:22:19 +0900

    feat: createImage method 구현
    - albumPage에 관한 image 저장 기능 담당 createImage method 구현

--- src/main/java/com/example/team2_be/album/page/AlbumPageService.java
+++ src/main/java/com/example/team2_be/album/page/AlbumPageService.java
@@ -3,6 +3,9 @@ package com.example.team2_be.album.page;
 import com.example.team2_be.album.Album;
 import com.example.team2_be.album.AlbumJPARepository;
 import com.example.team2_be.album.page.dto.AlbumPageRequestDTO;
+import com.example.team2_be.album.page.dto.AlbumPageRequestDTO.AssetDTO;
+import com.example.team2_be.album.page.image.AlbumPageImage;
+import com.example.team2_be.album.page.image.AlbumPageImageJPARepository;
 import com.example.team2_be.core.error.exception.NotFoundException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -15,10 +18,12 @@ import org.springframework.stereotype.Service;
 public class AlbumPageService {
     private final AlbumJPARepository albumJPARepository;
     private final AlbumPageJPARepository albumPageJPARepository;
+    private final AlbumPageImageJPARepository albumPageImageJPARepository;
 
     public void createPage(AlbumPageRequestDTO requestDTO, Long albumId) throws JsonProcessingException {
         Map<String, Object> shapesMap = requestDTO.getShapes();
         Map<String, Object> bindingsMap = requestDTO.getBindings();
+        Map<String, AssetDTO> assetDTOMap = requestDTO.getAssets();
 
         ObjectMapper objectMapper = new ObjectMapper();
         String shapes = objectMapper.writeValueAsString(shapesMap);
@@ -29,12 +34,27 @@ public class AlbumPageService {
                 .shapes(shapes)
                 .bindings(bindings)
                 .build();
-        albumPageJPARepository.save(albumPage);
+        AlbumPage saveAlbumPage = albumPageJPARepository.save(albumPage);
+        createImage(saveAlbumPage, assetDTOMap);
     }
 
     private Album findAlbum(Long albumId) {
         Album album = albumJPARepository.findById(albumId)
                 .orElseThrow(() -> new NotFoundException("해당 id값을 가진 앨범을 찾을 수 없습니다. : " + albumId));
         return album;
+    }
+
+    private void createImage(AlbumPage albumPage, Map<String, AssetDTO> assetDTOMap) {
+        for(AssetDTO assetDTO : assetDTOMap.values()){
+            AlbumPageImage albumPageImage = AlbumPageImage.builder()
+                    .albumPage(albumPage)
+                    .assetId(assetDTO.getId())
+                    .fileName(assetDTO.getFileName())
+                    .type(assetDTO.getType())
+                    .xSize(assetDTO.getSize()[0])
+                    .ySize(assetDTO.getSize()[1])
+                    .build();
+            albumPageImageJPARepository.save(albumPageImage);
+        }
     }
 }
