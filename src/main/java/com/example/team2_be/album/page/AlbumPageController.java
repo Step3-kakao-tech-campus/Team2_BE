622f9f385c6509cbadb8e398c8fed077c0b5f73b
KimSehoon <shggm2000@gmail.com>
2023-11-04 23:32:05 +0900

    refactor: createPage method 인자 값 수정
    - 어떤 앨범인지 구분하기 위해 albumId 값을 인자로 받음

--- src/main/java/com/example/team2_be/album/page/AlbumPageController.java
+++ src/main/java/com/example/team2_be/album/page/AlbumPageController.java
@@ -5,6 +5,7 @@ import com.example.team2_be.core.utils.ApiUtils;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,9 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class AlbumPageController {
     private final AlbumPageService albumPageService;
 
-    @PostMapping("")
-    public ResponseEntity<ApiUtils.ApiResult> createPage(@RequestBody AlbumPageRequestDTO requestDTO) throws JsonProcessingException {
-        albumPageService.createPage(requestDTO);
+    @PostMapping("/{albumId}")
+    public ResponseEntity<ApiUtils.ApiResult> createPage(@RequestBody AlbumPageRequestDTO requestDTO, @PathVariable Long albumId) throws JsonProcessingException {
+        albumPageService.createPage(requestDTO, albumId);
         return ResponseEntity.ok(ApiUtils.success(null));
     }
 }
