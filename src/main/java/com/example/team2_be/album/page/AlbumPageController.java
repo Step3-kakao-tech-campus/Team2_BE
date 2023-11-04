58c7c855e5ca47c265f31c75a6e584521ec6d142
KimSehoon <shggm2000@gmail.com>
2023-11-05 02:29:27 +0900

    refactor: createPage 예외 변경
    - 이미지 업로드 과정에서 예외 처리 변경
    - JsonProcessingException -> IOException

--- src/main/java/com/example/team2_be/album/page/AlbumPageController.java
+++ src/main/java/com/example/team2_be/album/page/AlbumPageController.java
@@ -3,6 +3,7 @@ package com.example.team2_be.album.page;
 import com.example.team2_be.album.page.dto.AlbumPageRequestDTO;
 import com.example.team2_be.core.utils.ApiUtils;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,7 +19,7 @@ public class AlbumPageController {
     private final AlbumPageService albumPageService;
 
     @PostMapping("/{albumId}")
-    public ResponseEntity<ApiUtils.ApiResult> createPage(@RequestBody AlbumPageRequestDTO requestDTO, @PathVariable Long albumId) throws JsonProcessingException {
+    public ResponseEntity<ApiUtils.ApiResult> createPage(@RequestBody AlbumPageRequestDTO requestDTO, @PathVariable Long albumId) throws IOException {
         albumPageService.createPage(requestDTO, albumId);
         return ResponseEntity.ok(ApiUtils.success(null));
     }
