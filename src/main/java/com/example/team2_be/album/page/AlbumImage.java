b0f2a6d6edee9163eace9058b0fb629148059625
JB <73532407+Choi-Jungbin@users.noreply.github.com>
2023-11-05 16:46:15 +0900

    Update AlbumImage.java

--- src/main/java/com/example/team2_be/album/page/AlbumImage.java
+++ src/main/java/com/example/team2_be/album/page/AlbumImage.java
@@ -1,48 +1,48 @@
-// package com.example.team2_be.album.page;
+package com.example.team2_be.album.page;
 
-// import com.example.team2_be.BaseEntity;
-// import javax.persistence.Column;
-// import javax.persistence.Entity;
-// import javax.persistence.JoinColumn;
-// import javax.persistence.ManyToOne;
-// import javax.persistence.Table;
-// import lombok.Builder;
-// import lombok.Getter;
-// import lombok.NoArgsConstructor;
-// import lombok.ToString;
+import com.example.team2_be.BaseEntity;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
-// @Entity
-// @Table(name = "albumImage")
-// @Getter
-// @ToString
-// @NoArgsConstructor
-// public class AlbumImage extends BaseEntity {
-//     @ManyToOne
-//     @JoinColumn(name = "AlbumPage")
-//     private AlbumPage albumPage;
+@Entity
+@Table(name = "albumImage")
+@Getter
+@ToString
+@NoArgsConstructor
+public class AlbumImage extends BaseEntity {
+    @ManyToOne
+    @JoinColumn(name = "AlbumPage")
+    private AlbumPage albumPage;
 
-//     @Column(length = 128, nullable = false, unique = true)
-//     private String assetId;
+    @Column(length = 128, nullable = false, unique = true)
+    private String assetId;
 
-//     @Column(length = 128, nullable = false, unique = true)
-//     private String fileName;
+    @Column(length = 128, nullable = false, unique = true)
+    private String fileName;
 
-//     @Column(length = 16, nullable = false)
-//     private String type;
+    @Column(length = 16, nullable = false)
+    private String type;
 
-//     @Column(nullable = false)
-//     private double xSize;
+    @Column(nullable = false)
+    private double xSize;
 
-//     @Column(nullable = false)
-//     private double ySize;
+    @Column(nullable = false)
+    private double ySize;
 
-//     @Builder
-//     public AlbumImage(AlbumPage albumPage, String assetId, String fileName, String type, double xSize, double ySize) {
-//         this.albumPage = albumPage;
-//         this.assetId = assetId;
-//         this.fileName = fileName;
-//         this.type = type;
-//         this.xSize = xSize;
-//         this.ySize = ySize;
-//     }
-// }
+    @Builder
+    public AlbumImage(AlbumPage albumPage, String assetId, String fileName, String type, double xSize, double ySize) {
+        this.albumPage = albumPage;
+        this.assetId = assetId;
+        this.fileName = fileName;
+        this.type = type;
+        this.xSize = xSize;
+        this.ySize = ySize;
+    }
+}
