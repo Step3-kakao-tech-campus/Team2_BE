2d09bd68d46e62e2e3813f0818c850866820c8bd
won0104 <98508955+won0104@users.noreply.github.com>
2023-11-07 01:22:26 +0900

    chore : 7일 이후 앨범페이지를 DB에서 삭제

--- src/main/java/com/example/team2_be/trash/TrashService.java
+++ src/main/java/com/example/team2_be/trash/TrashService.java
@@ -1,18 +1,19 @@
 package com.example.team2_be.trash;
 
 import com.example.team2_be.album.page.AlbumPage;
+import com.example.team2_be.album.page.AlbumPageJPARepository;
 import com.example.team2_be.core.error.exception.NotFoundException;
 import com.example.team2_be.trash.dto.TrashesFindResponseDTO;
 import com.example.team2_be.user.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.scheduling.TaskScheduler;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -20,7 +21,7 @@ import java.util.List;
 public class TrashService {
 
     private final TrashJPARepository trashJPARepository;
-    private final TaskScheduler taskScheduler;
+    private final AlbumPageJPARepository albumPageJPARepository;
 
     public TrashesFindResponseDTO findTrashes(Long albumId, Pageable pageable){
         Page<Trash> trashes = trashJPARepository.findAllByAlbumId(albumId, pageable);
@@ -40,7 +41,10 @@ public class TrashService {
     @Scheduled(cron = "0 59 23 * * ?")
     public void deleteTrash(){
         List<Trash> after7days = trashJPARepository.findTrashesToDelete();
-        trashJPARepository.deleteAllInBatch(after7days);
+        List<AlbumPage> deletePages = after7days.stream()
+                .map(Trash::getAlbumPage)
+                .collect(Collectors.toList());
+        albumPageJPARepository.deleteAllInBatch(deletePages);
     }
 
     @Transactional
